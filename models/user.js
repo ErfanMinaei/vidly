1fd525291d9fec6b57f2f977b14a2897af06760e
Erfan <erfanminaei9@gmail.com>
2025-10-29 20:56:48 +0330

    add joi password complexity in user

--- models/user.js
+++ models/user.js
@@ -1,39 +1,50 @@
 const Joi = require('joi');
+const passwordComplexity = require('joi-password-complexity');
 const mongoose = require('mongoose');
 
 // modeling:
 const usersSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        maxlength: 50,
-        minlength: 3
-    },
-    email: {
-        type: String,
-        required: true,
-        unique : true,
-        minlength: 15
-    },
-    password: {
-        type: String,
-        required: true,
-        maxlength: 1024,
-        minlength: 4
-    },
+  name: {
+    type: String,
+    required: true,
+    maxlength: 50,
+    minlength: 3
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 15
+  },
+  password: {
+    type: String,
+    required: true,
+    maxlength: 1024,
+    minlength: 4
+  },
 });
 
 const User = mongoose.model('user', usersSchema);
 
+// Password complexity options
+const complexityOptions = {
+  min: 8,
+  max: 30,
+  lowerCase: 1,
+  upperCase: 1,
+  numeric: 1,
+  symbol: 1,
+  requirementCount: 4,
+};
 
-//validate fuctions:
-
+// base schema for validation
 const baseUserSchema = Joi.object({
   name: Joi.string().min(3).max(50),
   email: Joi.string().email().min(5).max(255),
-  password: Joi.string().min(4).max(255)
+  password: passwordComplexity(complexityOptions)
 });
 
+// validation functions
 function validateCreate(user) {
   return baseUserSchema
     .fork(['name', 'email', 'password'], (field) => field.required())
@@ -43,8 +54,6 @@ function validateCreate(user) {
 function validateUpdate(user) {
   return baseUserSchema.validate(user);
 }
-
-
 
 module.exports = {
   User,
