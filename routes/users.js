d53b068295fb489b440000d6781884c875309a13
Erfan <erfanminaei9@gmail.com>
2025-10-29 20:28:19 +0330

    use lodash to send respons on users

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const mongoose = require('mongoose');
 const { User, validateUpdate, validateCreate } = require('../models/user');
@@ -18,13 +19,9 @@ router.post('/', async (req, res) => {
     if (user) return res.status(400).send('User already registered.');
     
     try {
-        const user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-        });
+        const user = new User( _.pick(req.body, ['name', 'email', 'password']));
         await user.save();
-        res.send(user);
+        res.send(_.pick(user, ['_id', 'name', 'email']));
     } catch (err) {
         console.error('Error saving user', err);
         res.status(500).send("Something went wrong while saving user.");
@@ -46,7 +43,7 @@ router.patch('/:id', async (req, res) => {
     if (req.body.password !== undefined) user.password = req.body.password;
 
     await user.save();
-    res.send(user);
+    res.send(_.pick(user, ['_id', 'name', 'email']));
 });
 
 
@@ -54,14 +51,14 @@ router.patch('/:id', async (req, res) => {
 router.delete('/:id',async(req, res)=>{
     const user = await User.findByIdAndDelete(req.params.id);
     if (!user) return res.status(404).send('The user with the given ID was not found.');
-    res.send(user);
+    res.send(_.pick(user, ['_id', 'name', 'email']));
 });
 
 // show the customer with the given ID:
 router.get('/:id', async (req, res)=>{
     const user = await User.findById(req.params.id);
     if (!user) return res.status(404).send('The user with the given ID was not found.');
-    res.send(user);
+    res.send(_.pick(user, ['_id', 'name', 'email']));
 });
 
 module.exports = router;
