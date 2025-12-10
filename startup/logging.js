25fe331e86071699e9c8bfbe9e4be2b413fe4fc5
Erfan <erfanminaei9@gmail.com>
2025-12-10 03:44:57 +0330

    show logs on the console (colorized and pertty)

--- startup/logging.js
+++ startup/logging.js
@@ -3,35 +3,41 @@ require('winston-mongodb');
 require('express-async-errors');
 
 module.exports = function(){
+  winston.configure({
+    format: winston.format.combine(
+      winston.format.errors({ stack: true }),
+      winston.format.colorize(),
+      winston.format.simple()
+    )
+  });
 
-    //handle uncaughtException and unhandledRejection and log the messages on file and mongodb
-    winston.exceptions.handle(
-      new winston.transports.File({ filename: 'uncaughtException.log' }),
-      new winston.transports.MongoDB({
-        db: 'mongodb://localhost/vidly',
-        level: 'error'
-      })
-    );
-    
-    //convert unhandledRejection to a uncaughtException to let winston handle it
-    process.on("unhandledRejection", (ex) => {
-      throw ex;
-    });
-    
-    //to log errors in file named logfile
-    winston.add(new winston.transports.File({
-      filename: 'logfile.log',
-      level: 'error'
-    }));
-
-    // log to console 
-    winston.add(new winston.transports.Console({
-      format: winston.format.simple()
-    }));
-    
-    //to log errors in mongodb
-    winston.add(new winston.transports.MongoDB({
+  //handle uncaughtException and unhandledRejection and log the messages on file and mongodb
+  winston.exceptions.handle(
+    new winston.transports.Console({ handleExceptions: true }),
+    new winston.transports.File({ filename: 'uncaughtException.log' }),
+    new winston.transports.MongoDB({
       db: 'mongodb://localhost/vidly',
-      level: 'error',
-    }));
+      level: 'error'
+    })
+  );
+  
+  //convert unhandledRejection to a uncaughtException to let winston handle it
+  process.on("unhandledRejection", (ex) => {
+    throw ex;
+  });
+  
+  //to log errors in file named logfile
+  winston.add(new winston.transports.File({
+    filename: 'logfile.log',
+    level: 'error'
+  }));
+
+  // log to console 
+  winston.add(new winston.transports.Console({}));
+  
+  //to log errors in mongodb
+  winston.add(new winston.transports.MongoDB({
+    db: 'mongodb://localhost/vidly',
+    level: 'error',
+  }));
 };
